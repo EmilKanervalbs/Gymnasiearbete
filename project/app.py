4067acd2ad6e6c81d0c4c180a526773c6cf77deb
EmilKanervalbs <54535838+EmilKanervalbs@users.noreply.github.com>
2020-11-06 13:17:24 +0100

    fler snabbvyer och navbar dropdown

--- project/app.py
+++ project/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="web/static", static_url_path="", template_folder="web/templates")
 
+class Server():
+    def __init__(self):
+        self.userSessions = []
+
 @app.route("/")
 def index():
     return render_template("index.html")
