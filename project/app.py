ead2b82827d122e3e1d5a0666085cc7032c6cb1d
EmilKanervalbs <54535838+EmilKanervalbs@users.noreply.github.com>
2020-11-10 13:46:27 +0100

    mer routes i server

--- project/app.py
+++ project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__, static_folder="web/static", static_url_path="", template_folder="web/templates")
 
@@ -8,6 +8,21 @@ class Server():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return redirect("/startpage")
 
+@app.route("/startpage")
+def startpage():
+    return render_template("startsida.html")
+
+@app.route("/schedule")
+def schedule():
+    return "schema yes"
+
+@app.route("/news")
+def news():
+    return "nyheter ooo"
+
+@app.route("/assignments")
+def assignments():
+    return "yeah no"
     #sdsdddddddsdddsds
