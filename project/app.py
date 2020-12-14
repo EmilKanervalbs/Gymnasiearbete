1ebd0f7fcbc4dc40cbf8440e2cbfa752c94dde7e
EmilKanervalbs <54535838+EmilKanervalbs@users.noreply.github.com>
2020-12-14 15:31:12 +0100

    prov och uppgifter visas dynamiskt

--- project/app.py
+++ project/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, make_response, request
 
 import json
 
+import time
+
 app = Flask(__name__, static_folder="web/static", static_url_path="", template_folder="web/templates")
 
 class Server():
@@ -24,7 +26,7 @@ class Server():
 
 		for user in self.users:
 			for lesson in self.content["groups"][user["group"]]["lessons"]:
-				user["lessons"].append(self.content["classes"][lesson])
+				user["lessons"].append(self.content["courses"][lesson])
 
 server = Server()
 
@@ -59,53 +61,58 @@ def assignments():
 @app.route("/getnews/", methods=["GET"])
 @app.route("/getnews/<newsID>", methods=["GET"])
 def getnews(newsID=None):
-    user = None
+	user = None
 
-    if request.cookies.get("session") in server.userSessions:
-        # print("------------------------------------ok")
-        user = server.userSessions[request.cookies.get("session")]
+	if request.cookies.get("session") in server.userSessions:
+		# print("------------------------------------ok")
+		user = server.userSessions[request.cookies.get("session")]
         
-        group = user["group"]
+		group = user["group"]
 
-        print(user["group"])
+		# print(user["group"])
 
-        # for news in server.content["news"]:
+		currentTime = time.time()
+
+		# for news in server.content["news"]:
             
-            # print("--x--",news)
+			# print("--x--",news)
 
-        if newsID == None:
-            newsDelivery = {"news":[]}
+		if newsID == None:
+			newsDelivery = {"news":[]}
 
-            for news in server.content["news"]:
-                # print("------------------------",str(news["classes"]))
-                if len(newsDelivery) > 5:
-                    break
-                elif group not in news["classes"]:
-                    continue
-                else: 
-                    newsDelivery["news"].append(news.copy())
+			for news in server.content["news"]:
+				
+				# print("------------------------",str(news["classes"]))
+				if len(newsDelivery) > 5:
+					break
+				elif group not in news["classes"]:
+					continue
+				elif news["startDate"] > currentTime or news["endDate"] < currentTime:
+					continue
+				else:
+					newsDelivery["news"].append(news.copy())
 
-            for news in newsDelivery["news"]:
-                oldContent = news["content"]
-                newContent = ""
-                for i in range(len(oldContent)):
-                    newContent += oldContent[i]
-                    if i > 75:
-                        newContent += "..."
-                        break
-                news["content"] = newContent
+			for news in newsDelivery["news"]:
+				oldContent = news["content"]
+				newContent = ""
+				for i in range(len(oldContent)):
+					newContent += oldContent[i]
+					if i > 75:
+						newContent += "..."
+						break
+				news["content"] = newContent
 
-            return newsDelivery
-        else:
-            newsID = int(newsID)
-            print("news requested by ID")
-            for news in server.content["news"]:
-                if news["newsid"] == newsID:
-                    print("found news ID", newsID)
-                    return news
+			return newsDelivery
+		else:
+			newsID = int(newsID)
+			print("news requested by ID")
+			for news in server.content["news"]:
+				if news["newsid"] == newsID:
+					print("found news ID", newsID)
+					return news
 
-    print("---------------------------------nope")
-    return '{"news":["bajs"]}'
+	print("---------------------------------nope")
+	return '{"news":["bajs"]}'
 
 @app.route("/getuser")
 def getuser():
@@ -116,6 +123,39 @@ def getuser():
 
 		# for lesson in server.content["groups"][user["group"]]["lessons"]:
 
-		# 	user["lessons"].append(server.content["classes"][lesson])
+		# 	user["lessons"].append(server.content["courses"][lesson])
 
 		return server.userSessions[request.cookies.get("session")]
+
+@app.route("/getassignments")
+def getassignments():
+	print("requested assignments")
+	if request.cookies.get("session") in server.userSessions:
+		user = server.userSessions[request.cookies.get("session")].copy()
+		assignments = []
+#
+		currentTime = time.time()
+
+		for assignment in server.content["assignments"][user["group"]]:
+			if assignment["time"] < currentTime:
+				continue
+			assignment2 = assignment.copy()
+			assignment2["course"] = server.content["courses"][assignment2["course"]]["displayName"]
+			# print(assignment2)
+			assignments.append(assignment2)
+		
+		return {"assignments" : assignments}
+
+
+
+
+
+
+# dev route för att slippa starta om servern när man vill läsa om "databasen" (legit en .json fil)
+@app.route("/update")
+def update():
+	with open("temp_resources/news.json", "r") as zzz:
+			test = zzz.read()
+
+	server.content = json.loads(test)
+	return redirect("/")
--- project/web/static/js/index.js
+++ project/web/static/js/index.js
@@ -21,6 +21,12 @@ setTimeout(() => {
     updateTime();
 }, 1000 - currentTime.getMilliseconds());
 
+var getWeekday = (x) => {
+	let y = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"];
+
+	return y[x.getDay()];
+}
+
 let x = document.getElementById("news")
 // console.log(x)
 
@@ -67,7 +73,7 @@ var getNewsByID = async (id) => {
             console.log("response recieved");
             // console.log(resp);
 
-            // return resp.json();
+            return resp.json();
     })
         .then((news) => {
             // console.log(news);
@@ -106,6 +112,77 @@ var getUser = async () => {
 }
 
 getUser();
+
+
+var getAssignments = async () => {
+    console.log("requesting assignments");
+    let assignments = await fetch("/getassignments")
+        .then((resp) => {
+            console.log("response recieved");
+
+            return resp.json();
+    })
+        .then((news) => {
+            return news;
+	});
+	
+	console.log(assignments);
+
+
+	assignments.assignments.forEach((x) => {
+		let element = document.createElement("assignment-element");
+		
+		let date = new Date(x.time * 1000);
+		let currentTime = new Date();
+
+		let weekendDate = new Date(Math.floor((new Date().getTime()) / 86400000) * 86400000 + (7 - currentTime.getDay()) * 86400000 - 1).getTime();
+
+		let dueDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+
+		element.setAttribute("name", x.name);
+		element.setAttribute("course", x.course);
+		element.setAttribute("type", x.type);
+
+		let parentElement;
+
+		console.log(weekendDate)
+		console.log(date)
+
+		switch(x.type) {
+			case "exam":
+				if (weekendDate >= date.getTime()) {
+					parentElement = document.getElementsByClassName("assignment-wrapper")[2];
+					dueDate = `${getWeekday(date)} ${date.getDate()}/${date.getMonth() + 1}`;
+				}
+				else {
+					parentElement = document.getElementsByClassName("assignment-wrapper")[3];
+				}
+				break;
+			default:
+				if (weekendDate >= date.getTime()) {
+					parentElement = document.getElementsByClassName("assignment-wrapper")[0];
+					dueDate = `${getWeekday(date)} ${date.getDate()}/${date.getMonth() + 1}`;
+
+				}
+				else {
+					parentElement = document.getElementsByClassName("assignment-wrapper")[1];
+				}
+		}
+
+		element.setAttribute("due", dueDate);
+
+
+		parentElement.append(element);
+
+
+		console.log(x);
+	});
+
+	window.customElements.define("assignment-element", Assignment)
+
+};
+
+getAssignments();
 
 // var user = getUser().then(data => {
 // 	console.log(data);
@@ -168,7 +245,90 @@ class News extends HTMLElement {
     }
 }
 
+class Assignment extends HTMLElement {
+	constructor() {
+		super();
+		var shadowDOM = this.attachShadow({
+			mode: "open"
+		});
+
+		var LINK = document.createElement("link")
+        LINK.setAttribute("rel", "stylesheet")
+        LINK.setAttribute("href", "/css/normalize.css")
+
+        var LINK2 = document.createElement("link")
+        LINK2.setAttribute("rel", "stylesheet")
+        LINK2.setAttribute("href", "/css/style.css")
+    
+        shadowDOM.appendChild(LINK);
+        shadowDOM.appendChild(LINK2);
+
+
+		var assignmentName = this.getAttribute("name");
+        var course = this.getAttribute("course");
+        var type = this.getAttribute("type");
+        var due = this.getAttribute("due");
+
+        var TABLE = document.createElement("table");
+        TABLE.className = "newstable";
+
+        var TBODY = document.createElement("tbody");
+        var TR = document.createElement("tr");
+
+		let displayType = "";
+
+		switch(type) {
+			case "assignment":
+				displayType = "Inlämningsuppgift";
+				break;
+			case "verbal":
+				displayType = "Muntlig uppgift";
+				break;
+			case "exam":
+				displayType = "Prov";
+				break;
+		}
+
+        var ASSIGNMENT_NAME = document.createElement("th");
+        ASSIGNMENT_NAME.innerText = assignmentName;
+
+		var FIRST_ROW = document.createElement("td");
+
+        var COURSE_NAME = document.createElement("div");
+		COURSE_NAME.innerText = course;
+
+		var DUE_DATE = document.createElement("div");
+		DUE_DATE.innerText = due
+
+		FIRST_ROW.append(COURSE_NAME);
+		FIRST_ROW.append(DUE_DATE);
+
+
+		var SECOND_ROW = document.createElement("td");
+
+		var TYPE = document.createElement("div");
+		TYPE.innerText = displayType;
+
+		SECOND_ROW.append(TYPE);
+		
+		
+
+        TR.append(ASSIGNMENT_NAME);
+        TR.append(FIRST_ROW);
+        TR.append(SECOND_ROW);
+
+        TBODY.append(TR);
+        TABLE.append(TBODY);
+
+		shadowDOM.append(TABLE);  
+		
+		console.log(TABLE);
+
+	}
+}
+
 document.getElementById("news").addEventListener("click", async (e) => { // ifall man clickar på en nyhet
+	console.log(e.target);
     if (e.target.nodeName == "NEWS-ELEMENT"){
         // console.log(e.target.getAttribute("news-id"));
         const POPUP = document.getElementById("popup");
@@ -182,6 +342,42 @@ document.getElementById("news").addEventListener("click", async (e) => { // ifal
         // date.setTime(news.startDate);
         POPUP.querySelector("h4").innerText = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${lengthen(date.getHours())}:${lengthen(date.getMinutes())}`
 
+
+        POPUP.classList.add("visible");
+
+    }
+});
+
+
+
+document.getElementById("assignments").addEventListener("click", async (e) => { // ifall man clickar på en nyhet
+	console.log(e.target);
+    if (e.target.nodeName == "ASSIGNMENT-ELEMENT"){
+        const POPUP = document.getElementById("popup");
+		let target = e.target;
+		
+        POPUP.querySelector("h2").innerText = target.getAttribute("name");
+        POPUP.querySelector("h3").innerText = target.getAttribute("course");
+        POPUP.querySelector("p").innerText = target.getAttribute("type");
+        
+		POPUP.querySelector("h4").innerText = target.getAttribute("due")
+
+        POPUP.classList.add("visible");
+
+    }
+});
+
+document.getElementById("exams").addEventListener("click", async (e) => { // ifall man clickar på en nyhet
+	console.log(e.target);
+    if (e.target.nodeName == "ASSIGNMENT-ELEMENT"){
+        const POPUP = document.getElementById("popup");
+		let target = e.target;
+		
+        POPUP.querySelector("h2").innerText = target.getAttribute("name");
+        POPUP.querySelector("h3").innerText = target.getAttribute("course");
+        POPUP.querySelector("p").innerText = target.getAttribute("type");
+        
+		POPUP.querySelector("h4").innerText = target.getAttribute("due")
 
         POPUP.classList.add("visible");
 
